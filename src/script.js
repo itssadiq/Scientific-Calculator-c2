ff2a5617f42d5d633bbdf45ba485185cc4ecdf02
Sadiq Ali <63207255+itssadiq@users.noreply.github.com>
2024-06-01 20:26:55 +0500

    Uploaded files for scientific calculators
    - Basic structure using bootstrap.
    - Added functionality using javascript.
    
    Ps: uploading half built calculator and deploying it for testing purposes on mobile phones first.

---
+++ src/script.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById("calc-display");
+    const buttons = document.getElementsByClassName("btn");
+
+    let currentValue = "";
+
+    function evaluateResult() {
+      const convertedValue = currentValue
+        .replace("×", "*")
+        .replace("÷", "/")
+        .replace("%","*0.01")
+        .replace("sin", "Math.sin")
+        .replace("ln", "Math.log")
+        .replace("π", "Math.PI")
+        .replace("cos","Math.cos");
+      const result = eval(convertedValue);
+      currentValue = result.toString();
+      display.value = currentValue;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+      const button = buttons[i];
+      button.addEventListener("click", function () {
+        const value = button.innerText;
+
+        try {
+          if (value == "AC") {
+          currentValue = "";
+          display.value = currentValue;
+        } else if (value == "=") {
+          evaluateResult();
+        } else {
+          currentValue += value;
+          display.value = currentValue;
+        }
+        } catch (error) {
+          console.error(error);
+          currentValue = "ERROR";
+          display.value = currentValue;
+        }
+        
+      });
+    }
+  });
